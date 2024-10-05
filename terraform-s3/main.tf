945cc7a9cb68e64eb2c47f132c13eb79200d1c67
Harbeylefty <harbeysegun@gmail.com>
2024-10-05 11:03:04 +0100

    modified workflow

--- terraform-s3/main.tf
+++ terraform-s3/main.tf
@@ -43,16 +43,18 @@ resource "aws_s3_object" "index" {
   bucket       = aws_s3_bucket.mybucket.id
   key          = "index.html"
   source       = "index.html"
-  acl          = "public-read"
   content_type = "text/html"
+
+  acl = "public-read"
 }
 
 resource "aws_s3_object" "error" {
   bucket       = aws_s3_bucket.mybucket.id
   key          = "error.html"
   source       = "error.html"
-  acl          = "public-read"
   content_type = "text/html"
+
+  acl = "public-read"
 }
 
 # Setup website 
